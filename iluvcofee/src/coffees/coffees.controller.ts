4adb069b1212dd3ec2b5ca7f40b717ef8a6752dd
numunna <numunna@cisco.com>
2020-11-04 22:55:35 -0500

    Added rest endpoint

---
+++ iluvcofee/src/coffees/coffees.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { response } from 'express';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees') // ties /coffees url to this controller
+export class CoffeesController {
+
+    constructor(
+        private readonly coffeesService: CoffeesService
+    ){}
+
+    @Get('flavors') // nested urls
+    findAll(@Query() paginationQuery) {
+        // const { limit, offset } = paginationQuery;
+        return this.coffeesService.findAll();
+    }
+
+    @Get(':id') // nested urls
+    findOne(@Param('id') id: string) {
+        return this.coffeesService.findOne(id);
+    }
+
+    @Post() // nested urls
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        console.log(createCoffeeDto instanceof CreateCoffeeDto);
+        return this.coffeesService.create(createCoffeeDto);
+    }
+
+    @Patch(':id') // nested urls
+    update(@Param('id') id: string, @Body() updateCOffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCOffeeDto);
+    }
+
+    @Delete(':id') // nested urls
+    remove(@Param('id') id: string) {
+        return this.coffeesService.remove(id);
+    }
+}
