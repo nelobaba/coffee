8d8c56de85fee82bc272d30852abafbb14289fa7
Nelson Umunna <nelo_baba@Nelsons-MacBook-Pro.local>
2020-11-09 21:18:20 -0500

    Database integration

--- iluvcofee/src/coffees/coffees.module.ts
+++ iluvcofee/src/coffees/coffees.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from '../events/entities/event.entity'
+import { Coffee } from './entity/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module(
     {
         controllers: [CoffeesController],
         providers: [CoffeesService],
-        exports: []
+        exports: [],
+        imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])] // used to register entities for a domaain
     }
 )
 export class CoffeesModule {}
